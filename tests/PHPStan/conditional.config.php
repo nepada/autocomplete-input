0c2e78378a1fd680fcd8c9d199500e2887e6e01c
Petr Morávek <petr@pada.cz>
2025-01-11 20:39:17 +0100

    Bump PHPStan to 2.x

--- tests/PHPStan/conditional.config.php
+++ tests/PHPStan/conditional.config.php
@@ -4,12 +4,12 @@ declare(strict_types = 1);
 use Composer\InstalledVersions;
 use Composer\Semver\VersionParser;
 
-$config = [];
+$config = ['parameters' => ['ignoreErrors' => []]];
 
 if (InstalledVersions::satisfies(new VersionParser(), 'nette/component-model', '<3.1')) {
     $config['parameters']['ignoreErrors'][] = [
         'message' => '#^Method Nepada\\\\AutocompleteInput\\\\AutocompleteInput\\:\\:getUniqueId\\(\\) should return string but returns string\\|null\\.$#',
-        'path' => '../../src/AutocompleteInput/AutocompleteInput.php',
+        'path' => __DIR__ . '/../../src/AutocompleteInput/AutocompleteInput.php',
         'count' => 1,
     ];
 }
