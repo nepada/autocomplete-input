694507721d46718072c4228b93f3c114dc2a2411
Petr Morávek <petr@pada.cz>
2026-04-11 12:37:01 +0200

    Update phpstan baseline

--- tests/PHPStan/conditional.config.php
+++ tests/PHPStan/conditional.config.php
@@ -14,4 +14,12 @@ if (InstalledVersions::satisfies(new VersionParser(), 'nette/component-model', '
     ];
 }
 
+if (! InstalledVersions::satisfies(new VersionParser(), 'nette/forms', '<=3.2.8')) {
+    $config['parameters']['ignoreErrors'][] = [
+        'message' => '~^Parameter \\#2 \\$callback of static method Nette\\\\Forms\\\\Container\\:\\:extensionMethod\\(\\) expects callable\\(Nette\\\\Forms\\\\Container\\)\\: mixed, Closure\\(Nette\\\\Forms\\\\Container, int\\|string, Nette\\\\Utils\\\\Html\\|string\\|null, callable, int\\|null\\=\\)\\: Nepada\\\\AutocompleteInput\\\\AutocompleteInput given\\.$~',
+        'path' => __DIR__ . '/../../src/Bridges/AutocompleteInputForms/ExtensionMethodRegistrator.php',
+        'count' => 1,
+    ];
+}
+
 return $config;
